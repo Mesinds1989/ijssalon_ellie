514182dce56e6231f836f6dc4da0b5ebf294e192
Maurice Taling <maurice.taling@gmail.com>
2026-05-27 08:05:46 +0200

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,21 @@
+prijzen={
+    "aardbei": 3,
+    "vanille": 4,
+    "Chocolade": 5,
+    
+}
+
+aanbieding = prijzen["aardbei"]*0.8
+reclame_tekst=(f"vandaag in de aanbieding: vanille-ijs, 1 liter - slechts€{aanbieding}")
+reclame_tekst2=reclame_tekst[:61]
+#print(reclame_tekst.index("0"))
+reclame_tekst3=reclame_tekst2.upper()
+reclame_tekst4=reclame_tekst3.split()
+print(reclame_tekst4)
+
+for el in reclame_tekst4:
+    if (len(el)) <4:
+        print(el.lower())
+        
+    if (len(el))> 3:
+        print(el)
