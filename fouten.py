782063950060577b436dba953aa77e1c5e3c4140
Maurice Taling <maurice.taling@gmail.com>
2026-05-30 10:46:27 +0200

    fouten.py aangepast naar vraag docent

--- fouten.py
+++ fouten.py
@@ -1,25 +1,26 @@
 import math
 def discriminant(a,b,c):
     try:
+        D1=0
+        D2=0
         D1=(-b+math.sqrt(b**2-4*a*c))/(2*a)
         D2=(-b-math.sqrt(b**2-4*a*c))/(2*a)
         uitvoer=[D1,D2]
 
     except:
-        uitvoer="  "
+        D1="geen oplossing"
+        D2="geen oplossing"
+        uitvoer=[D1,D2]
 
     return (uitvoer)
 
 a=int (input("wat is a?"))
 b=int (input("wat is b?"))
 c=int (input("wat is c?"))
+
 uitkomst=discriminant(a,b,c)
-'''
-if uitkomst >0:
-    D1=uitkomst[0]
-else:
-    D1="geen oplossing"
-'''    
+
+
 D1=uitkomst[0]
 D2=uitkomst[1]
 
