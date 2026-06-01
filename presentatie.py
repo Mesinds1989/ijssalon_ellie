8f75e59d1f75935ce24a28d09cacce2b5552edb9
Maurice Taling <maurice.taling@gmail.com>
2026-06-01 15:02:47 +0200

    verwijderde onnodige code in functie presenteer

--- presentatie.py
+++ presentatie.py
@@ -3,8 +3,7 @@
 def presenteer(dict,totaal):
     for k,v in dict.items():
         print(f"{k} : {v}")
-    tel=0
-    tel+=v
+    
     print("==========================")
     totaal=(f"Totaal : {totaal}")
     return totaal
